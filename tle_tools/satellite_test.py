42ae6c4ba6885d389e750a15ada6a2318dfa1d66
Christian Wygoda <christian.wygoda@satellitevu.com>
2024-05-30 20:37:24 +0200

    feat: add position method

--- tle_tools/satellite_test.py
+++ tle_tools/satellite_test.py
@@ -1,5 +1,24 @@
+from datetime import datetime, timezone
+
+from pytest import raises
+from shapely import Point
+
 from tle_tools.satellite import Satellite
 
 
-def test_satellite():
-    Satellite()
+def test_position_invalid_datetime(polar_tle):
+    sat = Satellite(polar_tle)
+    t = datetime(2024, 4, 19, 12, 0, 0, 0)
+
+    with raises(ValueError, match="datetime must be in utc"):
+        sat.position(t)
+
+
+def test_position(polar_tle):
+    sat = Satellite(polar_tle)
+    t = datetime(2024, 4, 19, 12, 0, 0, 0, timezone.utc)
+
+    pos = sat.position(t)
+    expected = Point(152.6226382884999, 78.18538506762289, 557934.9901695348)
+
+    assert pos.equals(expected)
